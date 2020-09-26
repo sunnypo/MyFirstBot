e018a6fadca80de102cf15a20f842820a5c390e8
sunnypo <siyuan0202@gmail.com>
2020-09-26 12:15:31 -0400

    add response and approach needed

--- guess.py
+++ guess.py
@@ -1,17 +1,20 @@
-import random
 from telegram.ext import CommandHandler
 
+def help():
+    return """
+    猜一个0-100之间的数字。You guessed a number from 0 - 100.
+/guess 查看现在的状态和获取帮助。Check your current status and get help.
+/guess number 输入number猜数字，看谁用的次数最少。Enter number and see who uses it the least often.
+    """
 
 def response_guess(update, context):
-    randomList = ["再走一遍老路也并不算坏事，-100XP", "命运终会犒赏努力的人，好好睡一觉吧，+1000XP", "奖赏一如既往，你为自己感到自豪，+500XP"]
-    number = random.randrange(0, 4)
-    if number == 3 :
-        randomXP = random.randrange(500, 1000)
-        message_reply = "争论不休, 迟迟没有结果。他们取了个平均数，+%sXP"%(randomXP)
+    print(context.args)
+    if len(context.args) == 0 :
+        update.message.reply_text(help())
     else:
-        message_reply = randomList[number]
-    msg = "现在是%s的封赏之时, 请命运指引我们到更光辉到明天吧 \n\"%s\" "%(update.message.from_user.first_name, message_reply)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
+        number = int(context.args[0])
+        update.message.reply_text("you said %s"%number)
+
 
 def add_dispatcher(dispatcher):
     response_guess_handler = CommandHandler('guess', response_guess)#文字
