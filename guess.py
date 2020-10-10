3d66b437f393c210a41661348d363b5a0fd1ed26
sunnypo <siyuan0202@gmail.com>
2020-10-10 11:30:06 -0400

    group support

--- guess.py
+++ guess.py
@@ -1,42 +1,46 @@
 import random
 from telegram.ext import CommandHandler
 
-answerNumber = random.randint(0, 100)
-member = {}
+
+games = {}
+
 
 def help():
     return """
-    猜一个0-100之间的数字。You guessed a number from 0 - 100.
+    猜一个0-1000之间的数字。You guessed a number from 0 - 1000.
 /guess 查看现在的状态和获取帮助。Check your current status and get help.
 /guess number 输入number猜数字，看谁用的次数最少。Enter number and see who uses it the least often.
     """
 
 def response_guess(update, context):
     global answerNumber
+    chatid = update.message.chat.id
     print(context.args)
-    print(answerNumber)
+    if not (chatid in games):
+        games[chatid] = {'answerNumber' : random.randint(0, 1000), 'member':{}}
+    print(games)  
     if len(context.args) == 0 :
         update.message.reply_text(help())
     else:
         if context.args[0].isdigit():
             number = int(context.args[0])
             userName = update.message.from_user.first_name
-            if userName in member:
-                member[userName] += 1
+            if userName in games[chatid]:
+                games[chatid]['member'] += 1
             else:
-                member[userName] = 1
-            update.message.reply_text("you said %s, %s"%(number, answerNumber))
-            if number == answerNumber:
+                games[chatid]['member'] = 1
+            update.message.reply_text("you said %s, %s"%(number, userName))
+            if number == games[chatid]['answerNumber']:
                 update.message.reply_text("%s is the right number"%number)
-                answerNumber = random.randint(0, 100)
-            elif number < answerNumber:
-                update.message.reply_text("%sfrom %s is too small"%(number,member))
-            elif number > answerNumber:
-                update.message.reply_text("%sfrom %s is too big"%(number,member))
+                answerNumber = random.randint(0, 1000)
+            elif number < games[chatid]['answerNumber']:
+                update.message.reply_text("%sfrom %s is too small"%(number,userName))
+            elif number > games[chatid]['answerNumber']:
+                update.message.reply_text("%sfrom %s is too big"%(number,userName))
         else :
             update.message.reply_text("your answer %s is not number"%context.args[0])
 
 
 def add_dispatcher(dispatcher):
-    response_guess_handler = CommandHandler('guess', response_guess)#文字
+    response_guess_handler = CommandHandler('guess1000', response_guess)#文字
     dispatcher.add_handler(response_guess_handler)
