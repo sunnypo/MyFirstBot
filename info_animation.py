538b42857d9ef98aabcd140d8a5d32af538b0d18
sunnypo <siyuan0202@gmail.com>
2021-01-02 09:44:57 -0500

    info picture without sneding pic again

--- info_animation.py
+++ info_animation.py
@@ -2,6 +2,7 @@
 
 from telegram import Update
 from telegram.ext import Dispatcher,CommandHandler,CallbackContext
+from telegram import BotCommand,Animation
 
 def get_info_animation(update : Update, context : CallbackContext):
     file_id = str("file_id = %s"%(update.message.reply_to_message.animation.file_id))
@@ -15,6 +16,8 @@ def get_info_animation(update : Update, context : CallbackContext):
     msg = "%s\n%s\n%s\n%s\n%s\n%s\n%s\n"%(file_id, file_unique_id, width, height, duration, file_name, file_size)
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
+    animation = Animation(update.message.reply_to_message.animation.file_id,update.message.reply_to_message.animation.file_unique_id,update.message.reply_to_message.animation.width,update.message.reply_to_message.animation.height,update.message.reply_to_message.animation.file_size)
+    update.message.reply_animation(animation = animation ,caption=f"{update.message.from_user.first_name}")
 
 def add_handler(dp: Dispatcher):
     dp.add_handler(CommandHandler(["info_animation"], get_info_animation))
